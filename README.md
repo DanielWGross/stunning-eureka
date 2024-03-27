db93129878b065611458dfb661f5544ec37a3ca7
Daniel Gross <dgross@instructors.2u.com>
2024-03-27 19:07:43 -0400

    add and configure eslint and prettier

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # stunning-eureka
 
-## Project Setup - 6:49PM
+## Project Setup - 6:49PM - 7:03PM
 
 - ✅ Github Repo
 - ✅ Create a basic Express server
-- Deployed to Heroku
+- ✅ Deployed to Heroku
+  - https://intense-coast-74645-40dca033a356.herokuapp.com/
 
-## Install & Configure Deps.
+## Install & Configure Deps. - 7:03PM - ?
 
+- Prettier
+- ESLint
+- Nodemon
 - Handlebars
 - Sequelize & MySQL
 - connect-sequelize-session
 - bcrypt
 - dotenv
-- Prettier
-- ESLint
-- Nodemon
 - Make sure it works on Heroku
 
 ## Add our first feature (vertical slice)
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 /** Imports */
-const express = require("express");
+const express = require('express');
 
 /** Express Configuration */
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 /** Routes */
-app.get("/", async (req, res) => {
-  res.send("Hello, World 🌎!")
+app.get('/', async (req, res) => {
+  res.send('Hello, World 🌎!');
 });
 
-app.listen(PORT, () => console.log("App is running!"));
+app.listen(PORT, () => console.log('App is running!'));
